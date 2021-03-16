eb4f0e1a690739c207d28c64de3bd360f951c162
WOOK HYUN <superwhyun@gmail.com>
2021-03-16 21:03:17 +0900

    adding build

---
+++ smartfarm/build/precache-manifest.9b44ac83cc5662cb8f4f4232c6b920ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f46786eab8e4f378988fb9265b12e2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b407f1fb7bcc1fec733",
+    "url": "/static/css/2.1252231c.chunk.css"
+  },
+  {
+    "revision": "5b793b087659d1ce7b65",
+    "url": "/static/css/main.4e0a62d3.chunk.css"
+  },
+  {
+    "revision": "0b407f1fb7bcc1fec733",
+    "url": "/static/js/2.77cb5003.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.77cb5003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b793b087659d1ce7b65",
+    "url": "/static/js/main.a570fa67.chunk.js"
+  },
+  {
+    "revision": "dbfe01488a15520447bc",
+    "url": "/static/js/runtime-main.bb98dc86.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/static/media/primeicons.b0f5d02f.eot"
+  }
+]);
